6c251dc2d0a3a9267df2a7e66c9f6fb87c7a29d1
Magila-Ferreira <magilamorganasf@hotmail.com>
2024-06-22 14:21:13 -0300

    arquivo es.php limpo

--- funcoes/idioma.php
+++ funcoes/idioma.php
@@ -1,12 +1,6 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-
 $acceptLang = ['es', 'en', 'pt']; 
 $lang = in_array($lang, $acceptLang) ? $lang : 'en';
             
--- funcoes/idiomaView.php
+++ funcoes/idiomaView.php
@@ -1,7 +1,6 @@
 <?php
 
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-
 $acceptLang = ['es', 'en', 'pt']; 
 $lang = in_array($lang, $acceptLang) ? $lang : 'en';
             
